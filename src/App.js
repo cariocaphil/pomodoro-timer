c7b4a986e3d10fc7542d36f17279b1a0033d6de0
cariocaphil <cariocaphil@gmail.com>
2023-03-07 14:44:35 +0100

    add reset logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
 
 function padTime(time) {
@@ -8,12 +8,17 @@ function padTime(time) {
 export default function App() {
   const [title, setTitle] = useState('Let the countdown begin!');
   const [timeLeft, setTimeLeft] = useState(25 * 60);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
   function startTimer() {
-    setInterval(() => {
+    if (intervalRef.current !== null) return;
+    setTitle('Clock is running');
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
       setTimeLeft((timeLeft) => {
         if (timeLeft >= 1) return timeLeft - 1;
-
+        resetTimer();
         return 0;
       });
     }, 1000);
@@ -21,6 +26,14 @@ export default function App() {
 
   const minutes = padTime(Math.floor(timeLeft / 60));
   const seconds = padTime(timeLeft - minutes * 60);
+
+  function resetTimer() {
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTitle('Another round');
+    setTimeLeft(25 * 60);
+    setIsRunning(false);
+  }
 
   return (
     <div className="app">
@@ -33,9 +46,9 @@ export default function App() {
       </div>
 
       <div className="buttons">
-        <button onClick={startTimer}>Start</button>
-        <button>Stop</button>
-        <button>Reset</button>
+        {!isRunning && <button onClick={startTimer}>Start</button>}
+        {isRunning && <button>Stop</button>}
+        <button onClick={resetTimer}>Reset</button>
       </div>
     </div>
   );
