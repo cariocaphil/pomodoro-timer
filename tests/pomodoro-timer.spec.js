39c3cbc28aee20643bebc2045fdaf8c2a57e8081
cariocaphil <cariocaphil@gmail.com>
2023-04-14 10:37:00 +0200

    refactor tests

--- tests/pomodoro-timer.spec.js
+++ tests/pomodoro-timer.spec.js
@@ -1,16 +1,18 @@
 const { test, expect } = require('@playwright/test');
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000');
+});
+
 test.describe('My React Pomodoro Timer App', () => {
   test('should have the correct title', async ({page}) => {
 
-    await page.goto('http://localhost:3000');
     const title = await page.title();
 
     expect(title).toBe('React App');
   });
 
   test('should have a clickable start button which then disappears', async ({ page }) => {
-    await page.goto('http://localhost:3000');
     const getStartedButton = page.getByRole('button', { name: 'Start' });
     
     await getStartedButton.click();
@@ -18,8 +20,7 @@ test.describe('My React Pomodoro Timer App', () => {
     await expect(getStartedButton).not.toBeVisible();
   });
 
-  test('should change the main message after clicking start, stop, and reset buttons an', async ({ page }) => {
-    await page.goto('http://localhost:3000');
+  test('should change the main message after clicking start, stop, and reset buttons', async ({ page }) => {
     const mainMessageElement = await page.locator('h2')
     await expect(mainMessageElement).toHaveText('Let the countdown begin!');
 
@@ -41,7 +42,6 @@ test.describe('My React Pomodoro Timer App', () => {
 
   test('timer should start ticking after clicking start button', async ({ page }) => {
     test.setTimeout(70000);
-    await page.goto('http://localhost:3000');
     await page.waitForSelector('.timer');
     const timerSpans = await page.$$('.timer span');
     const initialTimerValueMinutes= timerSpans[0]?.textContent();
